33527a5af90476ede7147d6ee2f8d9a33e1ee8a9
benmatott <benmatott@gmail.com>
2024-10-31 09:49:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-<script>
-    const circles = document.querySelectorAll('a');
-    circles.forEach(circle => {
-        circle.addEventListener('mouseover', () => {
-            circle.style.transform = 'scale(1.1)';
-        });
-        circle.addEventListener('mouseout', () => {
-            circle.style.transform = 'scale(1)';
-        });
+const circles = document.querySelectorAll('a');
+circles.forEach(circle => {
+    circle.addEventListener('mouseover', () => {
+        circle.style.transform = 'scale(1.1)';
     });
-</script>
+    circle.addEventListener('mouseout', () => {
+        circle.style.transform = 'scale(1)';
+    });
+});
